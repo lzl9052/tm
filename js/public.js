3e06919040b7ba4cc2c32551b9f141895874365e
假如我年少有为不自卑 <10465200+lzl9052@user.noreply.gitee.com>
2022-03-02 22:34:04 +0800

    new

--- js/public.js
+++ js/public.js
@@ -1,34 +1,82 @@
-$(function (){
-    $('.tb,.tbb').mouseover(function () {
-        $('.tb').addClass('shopping-listshow');
-        $('.tbb').addClass('tb2');
+$(function(){
+    $('.tb,.tbb').on({
+        mouseover:function (){
+            $('.tb').addClass('shopping-listshow');
+            $('.tbb').show();
+        },
+        mouseleave:function (){
+            $('.tb').removeClass('shopping-listshow');
+            $('.tbb').hide();
+        }
     });
-    $('.tb,.tbb').mouseleave(function () {
-        $('.tb').removeClass('shopping-listshow');
-        $('.tbb').removeClass('tb2');
+    $('.collection,.collect').on({
+        mouseover:function (){
+            $('.collection').addClass('shopping-listshow');
+            $('.collect').show();
+        },
+        mouseleave:function (){
+            $('.collection').removeClass('shopping-listshow');
+            $('.collect').hide();
+        }
     });
-    $('.collection,.collect').mouseover(function () {
-        $('.collection').addClass('shopping-listshow');
-        $('.collect').addClass('tb2');
+    $('.phone-edition,.phone,.phone-button').on({
+        mouseover:function (){
+            $('.phone').show();
+        },
+        mouseleave:function (){
+            $('.phone').hide();
+        }
+    })
+    $('.sz,.contact').on({
+        mouseover:function (){
+            $('.sz').addClass('shopping-listshow');
+            $('.contact').show();
+        },
+        mouseleave:function (){
+            $('.sz').removeClass('shopping-listshow');
+            $('.contact').hide();
+        }
     });
-    $('.collection,.collect').mouseleave(function () {
-        $('.collection').removeClass('shopping-listshow');
-        $('.collect').removeClass('tb2');
-    });
-    $('.phone-edition,.phone,.phone-button').mouseover(function () {
+    function toggleElevators(){
+        if($(document).scrollTop()>=$('.shopping-content').offset().top){
+            $('.elevators').stop().fadeIn();
+        }else {
+            $('.elevators').stop().fadeOut();
+        }
+    }
+    var flag=true;
+    $(window).scroll(function (){
+        if($(document).scrollTop()>300){
+            $('.goBack').show();
+        }else {
+            $('.goBack').hide();
+        }
+        toggleElevators();
 
-        $('.phone').addClass('tb2');
-    });
-    $('.phone-edition,.phone,.phone-button').mouseleave(function () {
+        if(flag){
+            $('.shopping-contents-title').each(function (i,ele){
+                if($(document).scrollTop()>=$(ele).offset().top){
+                    i=parseInt(i/4);
+                    $('.elevators li').eq(i).css('background-color','#ff780f').siblings().css('background-color','white');
+                }
+            })
+        }
 
-        $('.phone').removeClass('tb2');
     });
-    $('.sz,.contact').mouseover(function () {
-        $('.sz').addClass('shopping-listshow');
-        $('.contact').addClass('tb2');
-    });
-    $('.sz,.contact').mouseleave(function () {
-        $('.sz').removeClass('shopping-listshow');
-        $('.contact').removeClass('tb2');
-    });
+    $('.elevators li').click(function (){
+        flag=false;
+        $(this).css('background-color','#ff780f').siblings().css('background-color','white');
+        var index=$(this).index();
+        var current=$('.shopping-contents-title').eq(index*4).offset().top;
+        $('body,html').stop().animate({
+            scrollTop:current,
+        },function (){
+            flag=true;
+        });
+    })
+    $('.goBack').click(function (){
+        $('body,html').stop().animate({
+            scrollTop:0,
+        });
+    })
 });
